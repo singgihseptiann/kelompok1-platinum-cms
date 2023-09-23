bbe9c91f14759d71fdf1816184735e39a94df9c5
singgihseptiann <septiansinggih93@gmail.com>
2023-09-23 23:34:56 +0700

    test lagi

--- src/component/Test.js
+++ src/component/Test.js
@@ -3,7 +3,7 @@ import React from "react";
 function TestComponent() {
   return (
     <div className="text-center mt-5">
-      <h1 className="display-1 fw-bold">Semangat</h1>
+      <h1 className="display-1 fw-bold">Semangatttttttt</h1>
     </div>
   );
 }
