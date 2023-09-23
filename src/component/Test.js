814099d49496c8ddd6baeedd1104c76f4303a281
singgihseptiann <septiansinggih93@gmail.com>
2023-09-23 22:48:00 +0700

    test

---
+++ src/component/Test.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TestComponent() {
+  return (
+    <div className="text-center mt-5">
+      <h1 className="display-1 fw-bold">Semangat</h1>
+    </div>
+  );
+}
+
+export default TestComponent;
