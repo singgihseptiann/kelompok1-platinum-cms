6bb33a6862d225e5e900083e4b92baf13665d3a1
singgihseptiann <septiansinggih93@gmail.com>
2023-09-26 20:35:06 +0700

    beresin barchart

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 import React from "react";
-import TestComponent from "./component/Test";
-import CobaComponent from "./component/coba";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import BarChartComponent from "./component/barChart";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TestComponent />} />
+        <Route path="/" element={<BarChartComponent />} />
         <Route path="login" element={<Login />} />
       </Routes>
     </BrowserRouter>
