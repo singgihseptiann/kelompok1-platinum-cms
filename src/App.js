e1be32c39cfa64611946512f399227a7cbc89b02
singgihseptiann <septiansinggih93@gmail.com>
2023-09-29 20:15:46 +0700

    resolve conflict package-lock.json

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import BarChartComponent from "./component/barChart";
-import MyTable from "./component/myTable";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+      <Route path="" element={<BarChartComponent />} />
         <Route path="login" element={<Login />} />
       </Routes>
     </BrowserRouter>
