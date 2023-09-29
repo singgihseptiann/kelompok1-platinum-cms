ca9b3991abb8beec663d8a1156121d991ee84b2e
GulaminHalim <halimsrgr@gmail.com>
2023-09-29 00:49:17 -0700

    feature: Add Topbar and sidebar menu

--- src/App.js
+++ src/App.js
@@ -6,14 +6,19 @@ import React from "react";
 import TestComponent from "./component/Test";
 import CobaComponent from "./component/coba";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
+import Topbar from "./component/Topbar";
+import Sidebar from "./component/sidebar";
+import "./style/Styles.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TestComponent/>}/>
-        <Route path="login" element={<Login/>}/>
+        <Route path="/" element={<TestComponent />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
