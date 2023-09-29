2bc5d3f042da5c39011edfbf4283bde84e6666c8
singgihseptiann <septiansinggih93@gmail.com>
2023-09-29 20:18:52 +0700

    resolve conflict

--- src/App.js
+++ src/App.js
@@ -7,12 +7,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import BarChartComponent from "./component/barChart";
 
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="" element={<BarChartComponent />} />
+        <Route path="" element={<BarChartComponent />} />
         <Route path="login" element={<Login />} />
       </Routes>
     </BrowserRouter>
