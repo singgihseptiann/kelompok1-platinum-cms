195c588a18bdced3e9b5a75fb5d0976a333e7868
singgihseptiann <septiansinggih93@gmail.com>
2023-09-29 19:29:00 +0700

    delete testComponent

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 import React from "react";
-import TestComponent from "./component/Test";
-import CobaComponent from "./component/coba";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 
@@ -12,8 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TestComponent/>}/>
-        <Route path="login" element={<Login/>}/>
+        <Route path="login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
