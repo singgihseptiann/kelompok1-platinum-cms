c7d1cdd7e3f703c2cc4af1040207d242dc8bac77
GulaminHalim <halimsrgr@gmail.com>
2023-10-08 13:07:05 -0700

    Fix Sidebar and Topbar

--- src/component/Topbar.js
+++ src/component/Topbar.js
@@ -7,6 +7,30 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import { InputGroup } from "react-bootstrap";
 import Sidebar from "./sidebar";
 // import "../style/Styles.css";
+import React from "react";
+
+import { NavLink } from "react-router-dom";
+
+import { Breadcrumb } from "react-bootstrap";
+import {
+  CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarFooter,
+  CDBSidebarHeader,
+  CDBSidebarMenu,
+  CDBSidebarMenuItem,
+  //  CDBSidebarSubmenu,
+} from "cdbreact";
+
+import SideNav, {
+  Toggle,
+  NavItem,
+  NavIcon,
+  NavText,
+} from "@trendmicro/react-sidenav";
+
+// Be sure to include styles at some point, probably during your bootstraping
+import "@trendmicro/react-sidenav/dist/react-sidenav.css";
 
 function Topbar() {
   return (
@@ -16,12 +40,18 @@ function Topbar() {
           <InputGroup>
             <Form.Control
               type="search"
-              placeholder="Search"
               className="float-end"
               aria-label="Search"
+              placeholder="&#128270; Search"
+              color="#8A8A8A"
             />
 
-            <Button variant="outline-success">Search</Button>
+            <Button
+              variant="outline-info"
+              style={{ color: "#0D28A6", fontWeight: "bold" }}
+            >
+              Search
+            </Button>
           </InputGroup>
         </Form>
 
