6aea8e12fdf54f0a9a1823cba02866365e0dad36
singgihseptiann <septiansinggih93@gmail.com>
2023-11-03 13:16:50 +0700

    fix searh top bar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,4 @@
-// import "./App.css";
-// import "bootstrap/dist/css/bootstrap.css";
-// import React from "react";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Login from "./pages/Login";
-// import EditCars from "./pages/EditCars";
-// import AddCar from "./component/AddCar";
-// import DashboardPages from "./pages/Header/Dashboard";
-
-// import RentedCar from "./pages/dashboardpages/DasboardRentedOrder";
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <DashboardPages />
-//       <Routes>
-//         <Route path="/dashboard" element={<RentedCar />} />
-//         <Route path="login" element={<Login />} />
-//         <Route path="/edit-cars/:id" element={<EditCars />} />
-//         <Route path="/add-car" element={<AddCar />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
+import React from "react";
 import "./App.css";
 import { useRoutes } from "react-router";
 
