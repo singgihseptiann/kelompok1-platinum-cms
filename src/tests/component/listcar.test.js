03981a7c349fe37a9519f7e968f8fb6e0d3ac45c
fandhyokka <okkafandi@gmail.com>
2023-11-05 02:28:37 +0700

    added fetch data unit testing on listcar component

--- src/tests/component/listcar.test.js
+++ src/tests/component/listcar.test.js
@@ -1,30 +1,48 @@
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { renderWithProviders } from "../../mocks/redux";
 import ListCarComponent from "../../component/Listcar/listcar";
+import axios from "axios";
 
+// Fetching data test
+describe("ListCarComponent Fetching Data", () => {
+  it("fetches and sets car data in state", async () => {
+    const mockCarData = {
+      cars: [
+        { id: 1, name: "Car 1", price: 10000, category: "small" },
+        { id: 2, name: "Car 2", price: 15000, category: "medium" },
+      ],
+    };
+
+    jest.spyOn(axios, "get").mockResolvedValue({ data: mockCarData });
+
+    renderWithProviders(<ListCarComponent />);
+
+    await waitFor(() => {
+      expect(screen.getByText("Car 1")).toBeInTheDocument();
+      expect(screen.getByText("Car 2")).toBeInTheDocument();
+    });
+  });
+});
+
+// Loader test
 describe("ListCarComponent with Mock Server", () => {
   it("loader should be visible properly", async () => {
-    // act(() => {
     renderWithProviders(<ListCarComponent />);
-    // });
-
     await waitFor(async () => expect(await screen.findByTestId("loader")).toBeVisible());
   });
 
-  // it("handles car deletion properly", async () => {
-  //   render(<ListCarComponent />);
+  // button test
+  it("handles car deletion properly", async () => {
+    renderWithProviders(<ListCarComponent />);
 
-  //   const deleteButton = screen.getByText("Delete");
-  //   userEvent.click(deleteButton);
-
-  //   expect(screen.getByText("Menghapus Data Mobil")).toBeInTheDocument();
-
-  //   const confirmButton = screen.getByText("Ya");
-  //   userEvent.click(confirmButton);
-  //   // Additional assertions based on behavior after deleting data
-  // });
-
-  // button pake fireEvent.click
-  // input pake fireEvent.change
+    const deleteButton = screen.queryByText(/Delete/i);
+    if (deleteButton) {
+      userEvent.click(deleteButton);
+      // Additional assertions after clicking delete button
+    } else {
+      console.error("Delete button not found.");
+    }
+  });
 });
